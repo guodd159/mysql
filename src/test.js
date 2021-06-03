bd1f7a818f389226287f8a4c5165cb59946c1398
guodd159 <guodd159@gmail.com>
2021-06-03 11:46:25 +0800

    add 打印

--- src/test.js
+++ src/test.js
@@ -511,44 +511,50 @@ limit ${baseCnt},${inc}`)
 
 
 //创建表//创建表//创建表//创建表//创建表//创建表//创建表//创建表//创建表//创建表//创建表
-// createTable().then(res=>{
-//   console.log(res)
-// })
+createTable().then(res=>{
+  console.log(res)
+  process.exit(0)
+})
 // 插入数据到ori// 插入数据// 插入数据// 插入数据// 插入数据// 插入数据// 插入数据// 插入数据// 插入数据// 插入数据// 插入数据// 插入数据
-// insertData(100000).then(res => {
-//     console.log(res)
-// })
+insertData(100000).then(res => {
+  console.log("insert success",res)
+  process.exit(0)
+
+})
 
 // 初始化目标表// 初始化目标表// 初始化目标表// 初始化目标表// 初始化目标表// 初始化目标表// 初始化目标表// 初始化目标表// 初始化目标表
-// initDest().then(res => {
-//     console.log("update success", res)
-//     process.exit(0)
-// })
+initDest().then(res => {
+    console.log("init 目标表 success", res)
+    process.exit(0)
+})
 
 //一定时间内 不间断 推送数据到数据库//一定时间内 不间断 推送数据到数据库//一定时间内 不间断 推送数据到数据库//一定时间内 不间断 推送数据到数据库
-// simulationOrder(5 * 60 * 1000).then(res => {
-//     console.log(res)
-// })
+simulationOrder(5 * 60 * 1000).then(res => {
+  console.log("推送数据结束", res)
+  // console.log(res)
+  process.exit(0)
+
+})
 
 
 // 增量数据填充到目标表// 增量数据填充到目标表// 增量数据填充到目标表// 增量数据填充到目标表// 增量数据填充到目标表// 增量数据填充到目标表// 增量数据填充到目标表// 增量数据填充到目标表
 
-// pubDataIntoDest().then(res => {
-//     console.log(res)
-//     process.exit(0)
-// })
+pubDataIntoDest().then(res => {
+    console.log(res)
+    process.exit(0)
+})
 
 
 //随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori//随机更新ori
-// updateOriByRandom().then(res=>{
-//     console.log(res)
-// })
+updateOriByRandom().then(res=>{
+    console.log(res)
+})
 
 
 //////处理ori变化的数据 sync into dest//////处理ori变化的数据 sync into dest//////处理ori变化的数据 sync into dest//////处理ori变化的数据 sync into dest
-// updateDataIntoDest().then(res => {
-//     console.log(res)
-// })
+updateDataIntoDest().then(res => {
+    console.log(res)
+})
 
 
 
